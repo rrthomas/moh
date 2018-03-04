82f72b96a91fc15a1c3343533e7f4944493e2d80
Reuben Thomas <rrt@sc3d.org>
2018-03-04 14:43:01 +0000

    Spellcheck
    N.B. US English
    
    Did not fix all doubtful words, as some are not strictly a matter of
    spelling.
    
    spelling-list.txt is used for actual words, and LocalWords in
    mother_of_hydrogen.txt for non-words that are part of comments, e.g.
    commands.

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,3 +1,10 @@
-((text-mode . ((eval . (add-hook 'after-save-hook
+;; Adapted from https://stackoverflow.com/questions/4012321/how-can-i-access-the-path-to-the-current-directory-in-an-emacs-directory-variabl
+((nil . ((eval . (set (make-local-variable 'ispell-personal-dictionary)
+                      (expand-file-name
+                       "spelling-list.txt"
+                       (file-name-directory
+                        (let ((d (dir-locals-find-file ".")))
+                          (if (stringp d) d (car d)))))))))
+ (text-mode . ((eval . (add-hook 'after-save-hook
                                  (lambda () (shell-command "a2x --dblatex-opts \"-P latex.output.revhistory=0 --texstyle=moh\" -d book mother_of_hydrogen.txt &"))
                                  nil t)))))

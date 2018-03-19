14a62d844ab3e506ba94035d5e2c099d337b9a7f
Reuben Thomas <rrt@sc3d.org>
2018-03-19 15:43:55 +0000

    Emacs support: stop personal dictionary setting leaching into other buffers
    Setting it globally appears to cause problems with buffers that may be
    reused for multiple directories. So set it only for text-mode as a
    workaround.

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,10 +1,11 @@
 ;; Adapted from https://stackoverflow.com/questions/4012321/how-can-i-access-the-path-to-the-current-directory-in-an-emacs-directory-variabl
-((nil . ((eval . (set (make-local-variable 'ispell-personal-dictionary)
-                      (expand-file-name
-                       "spelling-list.txt"
-                       (file-name-directory
-                        (let ((d (dir-locals-find-file ".")))
-                          (if (stringp d) d (car d)))))))))
- (text-mode . ((eval . (add-hook 'after-save-hook
-                                 (lambda () (shell-command "a2x mother_of_hydrogen.txt &"))
-                                 nil t)))))
+((text-mode . ((eval . (progn
+                         (set (make-local-variable 'ispell-personal-dictionary)
+                              (expand-file-name
+                               "spelling-list.txt"
+                               (file-name-directory
+                                (let ((d (dir-locals-find-file ".")))
+                                  (if (stringp d) d (car d))))))
+                         (add-hook 'after-save-hook
+                                   (lambda () (shell-command "a2x mother_of_hydrogen.txt &"))
+                                   nil t))))))
